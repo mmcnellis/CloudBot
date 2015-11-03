11557334d94cc0919b07c40cc52a6e35b472854f
joriskcf <github@kcf.me>
2015-11-03 19:00:05 +0100

    remove duckhunt, google-->ddg

--- plugins/lmgtfy.py
+++ plugins/lmgtfy.py
@@ -4,10 +4,18 @@ from cloudbot import hook
 from cloudbot.util import web
 
 
-@hook.command("lmgtfy", "gfy")
-def lmgtfy(text):
-    """[phrase] - gets a lmgtfy.com link for the specified phrase"""
+# @hook.command("lmgtfy", "gfy")
+# def lmgtfy(text):
+#     """[phrase] - gets a lmgtfy.com link for the specified phrase"""
+#
+#     link = "http://lmgtfy.com/?q={}".format(requests.utils.quote(text))
+#
+#     return web.try_shorten(link)
 
-    link = "http://lmgtfy.com/?q={}".format(requests.utils.quote(text))
+@hook.command("lmgtfy", "gfy", "lmdtfy", "dfy")
+def lmgtfy(text):
+    """[phrase] - gets a lmddgtfy.net link for the specified phrase"""
+
+    link = "http://lmddgtfy.net//?q={}".format(requests.utils.quote(text))
 
     return web.try_shorten(link)

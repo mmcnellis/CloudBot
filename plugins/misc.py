27333bc08f82f67c03c73ab9d6f911f85ff74e7b
joriskcf <github@kcf.me>
2015-11-08 14:47:59 +0100

    started working on vucket functions, added .b12

--- plugins/misc.py
+++ plugins/misc.py
@@ -9,6 +9,12 @@ from cloudbot import hook
 def scarybon():
     return "☻"
 
+
 @hook.command
 def scaryboff():
     return "☺"
+
+
+@hook.command
+def b12(text, action):
+    action("hands " + text + " a bottle of B12")

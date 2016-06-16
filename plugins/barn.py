493bef6f9fb42e951a0a1fe3ece364ffa6ba5c18
mloy <molloy.mcnellis@gmail.com>
2016-06-16 12:13:03 -0400

    Make .barnupdate actually work

--- plugins/barn.py
+++ plugins/barn.py
@@ -1,40 +1,41 @@
 import json
+import os
 import urllib.request
 
 from cloudbot import hook
 
-burl = "http://barnivore.com/beer.json"
-wurl = "http://barnivore.com/wine.json"
-lurl = "http://barnivore.com/liquor.json"
+@hook.command("barnupdate", autohelp=False)
+def barnupdate():
+    data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+    suffix = '.json'
 
+    beer_path = os.path.join(data_dir, 'barnbeer' + suffix)
+    wine_path = os.path.join(data_dir, 'barnwine' + suffix)
+    liquor_path = os.path.join(data_dir, 'barnliquor' + suffix)
 
-def beer_update():
+    burl = "http://barnivore.com/beer.json"
+    wurl = "http://barnivore.com/wine.json"
+    lurl = "http://barnivore.com/liquor.json"
+
+# beer update
     response = urllib.request.urlopen(burl)
     str_response = response.readall().decode('utf-8')
     data = json.loads(str_response)
-    with open('../data/barnbeer.txt', 'w') as outfile:
+    with open(beer_path, 'w+') as outfile:
         json.dump(data, outfile)
-    return true
 
-def wine_update():
+# wine update
     response = urllib.request.urlopen(wurl)
     str_response = response.readall().decode('utf-8')
     data = json.loads(str_response)
-    with open('../data/barnwine.txt', 'w') as outfile:
+    with open(wine_path, 'w+') as outfile:
         json.dump(data, outfile)
-    return true
 
-def liquor_update():
+# liquor update
     response = urllib.request.urlopen(lurl)
     str_response = response.readall().decode('utf-8')
     data = json.loads(str_response)
-    with open('../data/barnliquor.txt', 'w') as outfile:
-        json.dump(data, outfile)
-    return true
+    with open(liquor_path, 'w+') as outfile:
+        json.dump(data, outfile)    
 
-@hook.command("barnupdate")
-def barnupdate():
-    beer_update()
-    wine_update()
-    liquor_update()
-    return true
+    return "Database updated."

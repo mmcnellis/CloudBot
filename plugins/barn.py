080e6e8da8bc748c5579e344e43084fdd70c3ca9
mloy <molloy.mcnellis@gmail.com>
2016-06-20 17:23:03 -0400

    Refactor barnupdate to append files

--- plugins/barn.py
+++ plugins/barn.py
@@ -1,41 +1,42 @@
+from fuzzywuzzy import fuzz
 import json
 import os
+from pprint import pprint
+
 import urllib.request
 
 from cloudbot import hook
 
+data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+suffix = '.json'
+barn_path = os.path.join(data_dir, 'barn' + suffix)
+
+burl = "http://barnivore.com/beer.json"
+wurl = "http://barnivore.com/wine.json"
+lurl = "http://barnivore.com/liquor.json"
+
+
 @hook.command("barnupdate", autohelp=False)
 def barnupdate():
-    data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-    suffix = '.json'
+# Append the json files and dump them to /data/barn.json
+    with open(barn_path, 'w+') as outfile:
 
-    beer_path = os.path.join(data_dir, 'barnbeer' + suffix)
-    wine_path = os.path.join(data_dir, 'barnwine' + suffix)
-    liquor_path = os.path.join(data_dir, 'barnliquor' + suffix)
+        response = urllib.request.urlopen(burl)
+        str_beer = response.readall().decode('utf-8')
+        beer = json.loads(str_beer)
+        data = beer
 
-    burl = "http://barnivore.com/beer.json"
-    wurl = "http://barnivore.com/wine.json"
-    lurl = "http://barnivore.com/liquor.json"
+        response = urllib.request.urlopen(wurl)
+        str_wine = response.readall().decode('utf-8')
+        wine = json.loads(str_wine)
+        data.append(wine)
 
-# beer update
-    response = urllib.request.urlopen(burl)
-    str_response = response.readall().decode('utf-8')
-    data = json.loads(str_response)
-    with open(beer_path, 'w+') as outfile:
+        response = urllib.request.urlopen(lurl)
+        str_liquor = response.readall().decode('utf-8')
+        liquor = json.loads(str_liquor)
+        data.append(liquor)
+
         json.dump(data, outfile)
-
-# wine update
-    response = urllib.request.urlopen(wurl)
-    str_response = response.readall().decode('utf-8')
-    data = json.loads(str_response)
-    with open(wine_path, 'w+') as outfile:
-        json.dump(data, outfile)
-
-# liquor update
-    response = urllib.request.urlopen(lurl)
-    str_response = response.readall().decode('utf-8')
-    data = json.loads(str_response)
-    with open(liquor_path, 'w+') as outfile:
-        json.dump(data, outfile)    
 
     return "Database updated."
+
